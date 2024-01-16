94e9dbb0304b1f9235b5714c4702308b3e3bfe3e
blueoption <mmartin@blueoption.io>
2024-01-16 21:04:41 +0100

    Remove Brand icon

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> BlueOption ⚛️ </span>
+        <span> BlueOption</span>
       </a>
     </span>
   )
